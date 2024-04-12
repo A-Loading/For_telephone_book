f41413ea9ef3ecb374d155b0eab456928d27c3b0
Лобашов Андрей <a.lobashov@yanex.ru>
2024-04-13 01:36:47 +0300

    new file:   PS.sh 	modified:   s.py

---
+++ PS.sh
@@ -0,0 +1,7 @@
+echo -e "\033[0;36m добавляем к индексации \033[0m"
+git add .
+git status
+echo -e "\033[0;36m комит \033[0m"
+git commit
+echo -e "\033[0;36m отправка \033[0m"
+git push
--- s.py
+++ s.py
@@ -1,10 +1,19 @@
+import os
 import json
 import sqlite3
 from easygui import *
-import os
+
 
 DATA_JSON = "tel.json"
 DATA_SQL = "telSQLLITE.bd"
+
+
+
+def save_sql(data) : 
+    print('sql заглушка')
+
+def load_sql(data) : # load_sql заглушка
+    pass
 
 def save_js(phonebook) :
     with open(DATA_JSON, "w", encoding='utf-8') as write_file:
@@ -17,59 +26,64 @@ def load_js() :
 
 def print_key(data):
     atr = list(data.keys())
-    print(*atr)
+    # print(*atr)
     return choicebox("Выберите абонента", "Главная форма", atr)
 
-def search(key):
+def search(data):
     pass
     
-phonebook = {
-    "милиция": { 'phones': ['02'], 
-                 'birthday': ['-'], 
-                 'email': ['-'] } ,
-    "скорая помощь": { 'phones': ['03'], 
-                  'birthday': ['-'], 
-                  'email': ['-'] } ,
-    "пожарная охрана": { 'phones': ['01'], 
-                  'birthday': ['-'], 
-                  'email': ['-'] }
-             }
-'''{
-    "дядя Ваня": { 'phones': [123], 
-                  'birthday': ['01.01.1960'], 
-                  'email': ["vanya@mail.ru"] } ,
-    "дядя Вася": { 'phones': [22222], 
-                  'birthday': ['01.01.2000'], 
-                  'email': ["vanya@mail.ru"] } 
-}'''
-
-
-
-# if not os.path.exists(DATA_JSON):
-    # save_js(phonebook)
-# data = load_js()
-# 
-# print(data['дядя Ваня'] ['phones'][0])  
-# print( print_key(data) )
-
+def import_():
+    if not os.path.exists(DATA_JSON):
+        print('ERR ↓•JSON•↓')
+        msgbox("""Ошибка загрузки файла tel.json 
+Бует создан шаблоный tel.json""", "Ошибка Импорта", ok_button="Пуск")
+                
+        phonebook = {
+                    "милиция": { 'phones': ['02'], 
+                                 'birthday': ['-'], 
+                                 'email': ['-'] } ,
+                    "скорая помощь": { 'phones': ['03'], 
+                                  'birthday': ['-'], 
+                                  'email': ['-'] } ,
+                    "пожарная охрана": { 'phones': ['01'], 
+                                  'birthday': ['-'], 
+                                  'email': ['-'] }
+                 }
+        save_js(phonebook)
+        print('[√] ↑•JSON•↑')
+    return load_js()
 
 
 def main_menu():
-    menu=['Открыть справочник', 'Импортировать', 'Экспартировать', 'Выход']
+    menu=['Открыть справочник', 'Импортировать', 'Экспортировать', 'Выход']
     data=0
     if os.path.exists(DATA_SQL):
-        # load_sql
-        print("sql")
+        load_sql() # load_sql заглушка
     else:
+        print('ERR ↓•SQL•↓')
         msgbox("""Ошибка подключения к Базе Данных
-        Импорт будет произведен из файла tel.json""","Ошибка БД", ok_button="Пуск")
-        if not os.path.exists(DATA_JSON):
-            msgbox("""Ошибка загрузки файла tel.json 
-            Бует создан шаблоный tel.json""", "Ошибка Импорта", ok_button="Пуск")
-            save_js(phonebook)
-        data = load_js()
-    print(data)
+Загрузка будет произведена из файла tel.json""","Ошибка БД", ok_button="Пуск")
+        data = import_()
+        print('[√] load •data•')
+    print('♥♦♣♠',data)
+    choice = 0
+    while (choice != "Выход") and (choice != None):
+        choice = choicebox("Меню", "Телефоная книга | Главное меню", menu)
+        print(choice)
+        if choice == 'Открыть справочник':
+            data = search(data)
+        elif choice == 'Импортировать':
+            data = import_()
+            print('[√] load •data•')
+        elif choice == 'Экспортировать':
+            save_js(data)
+            print('[√] ↑•JSON•↑')
+    save_sql(data)
+        
     
     
-
+    
+print('Запуск телефоной книги')
 main_menu()
+
+
